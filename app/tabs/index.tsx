990963fb06747b52a7b31ae212adb9421fb711c7
Ratko Mitrasinovic <ratko.mitrasinovic@combinedratio.com>
2026-01-16 21:12:04 +0100

    add buttons and different styles

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,6 +1,6 @@
-import { Image } from 'expo-image';
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
 import { StyleSheet, View } from 'react-native';
-
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -9,7 +9,11 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme='primary' />
+        <Button label="Take a photo" />
       </View>
     </View>
   );
@@ -28,5 +32,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
